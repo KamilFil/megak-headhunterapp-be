05c19eba231438b55d75035795b48462ce62a969
Kamil F <kamilfprog@gmail.com>
2022-07-19 15:53:21 +0200

    Create first module, controller, service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HrController } from './hr/hr.controller';
+import { HrModule } from './hr/hr.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [HrModule],
+  controllers: [AppController, HrController],
   providers: [AppService],
 })
 export class AppModule {}
