1cfdf6a772e9fdade132c4d3af5338a42861342b
Kamil F <kamilfprog@gmail.com>
2022-07-20 23:27:35 +0200

    Create brench for task "create-types" and create types fot student in types/student.

--- types/student/student-entity.ts
+++ types/student/student-entity.ts
@@ -1,3 +1,20 @@
 export interface StudentEntity {
-    name:string;
+  email: string;
+  tel: number;
+  firstName: string;
+  lastName: string;
+  githubUsername: string;
+  portfolioUrls: string;
+  projectUrls: string;
+  bio: string;
+  expectedTypeWork: string;
+  targetWorkCity: string;
+  expectedContractType: string;
+  expectedSalary: number;
+  canTakeApprenticeship: string;
+  monthsOfCommercialExp: number;
+  education: string;
+  workExperience: string;
+  courses: string;
+  active: number;
 }
