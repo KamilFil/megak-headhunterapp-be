14cae3287fc43e7fbb9ed28f6e68d668b7ea8700
sml87 <szewczyk.home@gmail.com>
2022-07-31 15:02:47 +0200

    cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { handleError } from './utils/errors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,7 @@ async function bootstrap() {
     origin: 'http://localhost:3000/',
     credentials: true,
   });
+  app.use(handleError);
   await app.listen(3001);
 }
 bootstrap();
