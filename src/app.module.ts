271b439fbe4012d6a2f47caf0a6dae8da1172adb
Kamil F <kamilfprog@gmail.com>
2022-08-04 18:49:53 +0200

    - Install @nestjs/passport passport passport-jwt - Generate Auth module (controller, service)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from './config/db.config';
 import { StudentModule } from './student/student.module';
 import { HrUserModule } from './hr-user/hr-user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dbConfig),
     StudentModule,
     HrUserModule,
+    AuthModule,
     // HrModule,
   ],
   controllers: [AppController /* HrController */],
